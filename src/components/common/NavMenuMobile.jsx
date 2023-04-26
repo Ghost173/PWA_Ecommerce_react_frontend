12d2fcfd5b0a3793d9269dd32dfc1a5409050a22
ghost173 <jpirakas007@gmail.com>
2023-04-27 00:52:19 +0530

    menu mobile

--- src/components/common/NavMenuMobile.jsx
+++ src/components/common/NavMenuMobile.jsx
@@ -6,36 +6,70 @@ import Logo from '../../assets/images/easyshop.png'
 import { Link } from 'react-router-dom'
 
 class NavMenuMobile extends Component {
+
+
+  constructor() {
+    super();
+    this.state = {
+      SideNavState: "sideNavClose",
+      ContentOverState: "ContentOverlayClose"
+    }
+  }
+
+
+  MenuBarClickHandler = () => {
+    this.SideNavOpenClose();
+  }
+
+  ContentOverlayClickHandler = () => {
+    this.SideNavOpenClose();
+  }
+
+
+  SideNavOpenClose = () => {
+    let SideNavState = this.state.SideNavState;
+    let ContentOverState = this.state.ContentOverState;
+    if (SideNavState === "sideNavOpen") {
+      this.setState({ SideNavState: "sideNavClose", ContentOverState: "ContentOverlayClose" })
+
+    }
+    else {
+      this.setState({ SideNavState: "sideNavOpen", ContentOverState: "ContentOverlayOpen" })
+    }
+  }
+
+
   render() {
     return (
       <Fragment>
-        <div className='TopSectionDown'>
+        <div className="TopSectionDown">
 
 
-          <Container fluid={"true"} className='fixed-top shadow-sm p-2 mb-0 bg-white' >
+          <Container fluid={"true"} className="fixed-top shadow-sm p-2 mb-0 bg-white">
             <Row>
               <Col lg={4} md={4} sm={12} xs={12}>
 
-                <Button className='btn'><i className='fa fa-bars'></i></Button>
+                <Button onClick={this.MenuBarClickHandler} className="btn"><i className="fa fa-bars"></i>  </Button>
 
-                <Link to="/"><img src={Logo} className='nav-logo' /> </Link>
-                <Button className='cart-btn'><i className='fa fa-shopping-cart'></i> 3 Items</Button>
+                <Link to="/"> <img className="nav-logo" src={Logo} /> </Link>
+
+                <Button className="cart-btn"><i className="fa fa-shopping-cart"></i> 3 Items </Button>
               </Col>
 
-
             </Row>
+
           </Container>
 
-          <div className='sideNavOpen'>
-            <hr className='w-80' />
-            <div className='list-group'>
-              <a className='list-group-item nav-font nav-itemmenu list-group-item-action'><i className='fa m-2 fa-home'>Home</i></a>
-            </div>
+          <div className={this.state.SideNavState}>
+            sdfsdf
           </div>
 
-        <div className='ContentOverlayOpen'>
+          <div onClick={this.ContentOverlayClickHandler} className={this.state.ContentOverState}>
 
-        </div>
+          </div>
+
+
+
 
         </div>
       </Fragment>
