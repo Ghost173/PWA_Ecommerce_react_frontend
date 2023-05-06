ffe2fd4bfe0d9b606ccd77eebd88ec49e59accc4
ghost173 <jpirakas007@gmail.com>
2023-05-07 02:21:05 +0530

    cart UI

---
+++ src/components/cart/Cart.jsx
@@ -0,0 +1,136 @@
+import React, { Component, Fragment } from 'react'
+import { Navbar, Container, Row, Col, Button, Card } from 'react-bootstrap';
+import Product1 from '../../assets/images/product/product1.png'
+import Product2 from '../../assets/images/product/product2.png'
+import Product3 from '../../assets/images/product/product3.png'
+import Product4 from '../../assets/images/product/product4.png'
+
+class Cart extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Container>
+
+                    <div className="section-title text-center mb-55"><h2>Product Cart List</h2>
+                    </div>
+
+                    <Row>
+                        <Col className="p-1" lg={12} md={12} sm={12} xs={12} >
+                            <Card >
+                                <Card.Body>
+                                    <Row>
+                                        <Col md={3} lg={3} sm={6} xs={6}>
+                                            <img className="cart-product-img" src={Product1} />
+                                        </Col>
+
+                                        <Col md={6} lg={6} sm={6} xs={6}>
+                                            <h5 className="product-name">ASUS TUF A15 FA506IU Ryzen 7 4800H GTX</h5>
+                                            <h6> Quantity = 05 </h6>
+                                            <h6>Price = 05 x 100 = 5000$</h6>
+                                        </Col>
+
+                                        <Col md={3} lg={3} sm={12} xs={12}>
+                                            <input placeholder="2" className="form-control text-center" type="number" />
+                                            <Button className="btn btn-block w-100 mt-3  site-btn"><i className="fa fa-trash-alt"></i> Remove </Button>
+
+                                        </Col>
+                                    </Row>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="p-1" lg={12} md={12} sm={12} xs={12} >
+                            <Card >
+                                <Card.Body>
+                                    <Row>
+                                        <Col md={3} lg={3} sm={6} xs={6}>
+                                            <img className="cart-product-img" src={Product2} />
+                                        </Col>
+
+                                        <Col md={6} lg={6} sm={6} xs={6}>
+                                            <h5 className="product-name">ASUS TUF A15 FA506IU Ryzen 7 4800H GTX</h5>
+                                            <h6> Quantity = 05 </h6>
+                                            <h6>Price = 05 x 100 = 5000$</h6>
+                                        </Col>
+
+                                        <Col md={3} lg={3} sm={12} xs={12}>
+                                            <input placeholder="2" className="form-control text-center" type="number" />
+                                            <Button className="btn btn-block w-100 mt-3  site-btn"><i className="fa fa-trash-alt"></i> Remove </Button>
+
+                                        </Col>
+                                    </Row>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="p-1" lg={12} md={12} sm={12} xs={12} >
+                            <Card >
+                                <Card.Body>
+                                    <Row>
+                                        <Col md={3} lg={3} sm={6} xs={6}>
+                                            <img className="cart-product-img" src={Product3} />
+                                        </Col>
+
+                                        <Col md={6} lg={6} sm={6} xs={6}>
+                                            <h5 className="product-name">ASUS TUF A15 FA506IU Ryzen 7 4800H GTX</h5>
+                                            <h6> Quantity = 05 </h6>
+                                            <h6>Price = 05 x 100 = 5000$</h6>
+                                        </Col>
+
+                                        <Col md={3} lg={3} sm={12} xs={12}>
+                                            <input placeholder="2" className="form-control text-center" type="number" />
+                                            <Button className="btn btn-block w-100 mt-3  site-btn"><i className="fa fa-trash-alt"></i> Remove </Button>
+
+                                        </Col>
+                                    </Row>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="p-1" lg={12} md={12} sm={12} xs={12} >
+                            <Card >
+                                <Card.Body>
+                                    <Row>
+                                        <Col md={3} lg={3} sm={6} xs={6}>
+                                            <img className="cart-product-img" src={Product4} />
+                                        </Col>
+
+                                        <Col md={6} lg={6} sm={6} xs={6}>
+                                            <h5 className="product-name">ASUS TUF A15 FA506IU Ryzen 7 4800H GTX</h5>
+                                            <h6> Quantity = 05 </h6>
+                                            <h6>Price = 05 x 100 = 5000$</h6>
+                                        </Col>
+
+                                        <Col md={3} lg={3} sm={12} xs={12}>
+                                            <input placeholder="2" className="form-control text-center" type="number" />
+                                            <Button className="btn btn-block w-100 mt-3  site-btn"><i className="fa fa-trash-alt"></i> Remove </Button>
+
+                                        </Col>
+
+                                        <Col className="p-1" lg={12} md={12} sm={12} xs={12} >
+                                            <Card >
+                                                <Card.Body>
+                                                    <Row>
+                                                        <Col className='float-right' md={4} lg={4} sm={6} xs={6}>
+
+                                                            <h5> Total Item = 05 </h5>
+                                                            <h5>Total Price = 5000$</h5>
+                                                            <Button className="btn btn-block w-100 mt-3  site-btn"><i className="fa fa-shopping-cart"></i> CheckOut </Button>
+                                                        </Col>
+                                                    </Row>
+                                                </Card.Body>
+                                            </Card>
+                                        </Col>
+                                        
+                                    </Row>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </Fragment>
+        )
+    }
+}
+
+export default Cart
--- src/components/common/NavMenuMobile.jsx
+++ src/components/common/NavMenuMobile.jsx
@@ -50,7 +50,7 @@ class NavMenuMobile extends Component {
               <Col lg={4} md={4} sm={12} xs={12}>
                 <Button onClick={this.MenuBarClickHandler} className="btn" variant="light"><i className="fa fa-bars"></i>  </Button>
                 <Link to="/"> <img className="nav-logo" src={Logo} /> </Link>
-                <Button className="cart-btn"><i className="fa fa-shopping-cart"></i> 3 Items </Button>
+                <Link to="/cart" className="cart-btn"><i className="fa fa-shopping-cart"></i> 3 Items </Link>
               </Col>
 
             </Row>
