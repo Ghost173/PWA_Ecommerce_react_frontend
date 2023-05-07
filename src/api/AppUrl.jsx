1698c4eafb6cbb108e3078e3eec3c8da441324ae
ghost173 <jpirakas007@gmail.com>
2023-05-07 15:29:18 +0530

    track visitors

---
+++ src/api/AppUrl.jsx
@@ -0,0 +1,6 @@
+class AppUrl {
+  static BaseURL = "https://pwaadmin.mydevdemo.xyz/api"
+  static VisitorDetails = this.BaseURL+"/createvisitor"
+}
+
+export default AppUrl
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,11 +9,18 @@ import NavMenuMobile from '../components/common/NavMenuMobile'
 import HomeTopMobile from '../components/home/HomeTopMobile'
 import FooterDesktop from '../components/common/FooterDesktop'
 import FooterMobile from '../components/common/FooterMobile'
+import axios from 'axios'
+import AppUrl from '../api/AppUrl'
 
 class HomePage extends Component {
 
   componentDidMount() {
-    window.scroll(0, 0)
+    window.scroll(0, 0);
+    this.GetVisitorDetails();
+  }
+
+  GetVisitorDetails = ()=>{
+    axios.get(AppUrl.VisitorDetails).then().catch()
   }
 
   render() {
