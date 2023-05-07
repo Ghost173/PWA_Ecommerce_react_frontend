bf759bf9964e250ebc7724dfb79070f3d4a5c8ae
ghost173 <jpirakas007@gmail.com>
2023-05-07 19:26:15 +0530

    contact fetch with api

--- src/api/AppUrl.jsx
+++ src/api/AppUrl.jsx
@@ -1,6 +1,8 @@
 class AppUrl {
-  static BaseURL = "https://pwaadmin.mydevdemo.xyz/api"
+  static BaseURL = "http://127.0.0.1:8000/api"
   static VisitorDetails = this.BaseURL+"/createvisitor"
+  static contactpost = this.BaseURL+"/postcontact"
+
 }
 
 export default AppUrl
