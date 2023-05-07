71a3270920742170262357203139421ef5c55328
ghost173 <jpirakas007@gmail.com>
2023-05-08 01:24:48 +0530

    fetch site info from api

--- src/api/AppUrl.jsx
+++ src/api/AppUrl.jsx
@@ -2,6 +2,8 @@ class AppUrl {
   static BaseURL = "http://127.0.0.1:8000/api"
   static VisitorDetails = this.BaseURL+"/createvisitor"
   static contactpost = this.BaseURL+"/postcontact"
+  static allsiteinfo = this.BaseURL+"/allsiteinfo"
+
 
 }
 
