f7724da5dfaf2b88c77ad4a645bef842fe21ed5a
ghost173 <jpirakas007@gmail.com>
2023-05-27 00:32:56 +0530

    protected profile page

--- src/components/user/Profile.jsx
+++ src/components/user/Profile.jsx
@@ -2,38 +2,54 @@ import React, { Component, Fragment } from 'react'
 
 import axios from 'axios';
 import AppUrl from '../../api/AppUrl';
+import { Navigate } from 'react-router-dom';
 
 export class Profile extends Component {
 
     constructor() {
         super();
         this.state = {
-          UserDetails: []
+            UserDetails: []
         }
-        
-    }
-    
-    componentDidMount() {
-    const token = localStorage.getItem('token');
-      axios.get(AppUrl.UserData, {
-        headers: {
-            Authorization: `Bearer ${token}` // Include the token in the Authorization header
-          }
-      })
-      
-      
-      .then(response => {
-          this.setState({UserDetails:response.data})
-      }).catch(error => {
-    
-      })
 
-      console.log("userdata test"+ this.state.UserDetails)
     }
-    
+
+    componentDidMount() {
+
+        // Redirect to login if token is not present
+       
+        const token = localStorage.getItem('token');
+
+        if (!token) {
+            // Use your preferred method for redirecting to the login page
+            window.location.href = '/login';
+            return null; // Render nothing
+        }
+
+        axios.get(AppUrl.UserData, {
+            headers: {
+                Authorization: `Bearer ${token}` // Include the token in the Authorization header
+            }
+        }).then(response => {
+            this.setState({ UserDetails: response.data })
+        }).catch(error => {
+            // localStorage.removeItem('token');
+            // window.location.href = '/login';
+        })
+
+        console.log("userdata test" + this.state.UserDetails)
+    }
+
 
 
     render() {
+        const token = localStorage.getItem('token');
+
+        // Redirect to login if token is not present
+        if (!token) {
+            // Use your preferred method for redirecting to the login page
+            return <Navigate to={'/login'} />
+        }
         return (
             <Fragment>
                 <h1> User Profile Page </h1>
--- src/components/user/UserRegister.jsx
+++ src/components/user/UserRegister.jsx
@@ -1,10 +1,47 @@
 import React, { Component, Fragment } from 'react'
 import { Col, Container, Form, Row, Button } from 'react-bootstrap'
 import loginimg from '../../assets/images/login.png'
-import { Link } from 'react-router-dom'
+import { Link, Navigate } from 'react-router-dom'
+import AppUrl from '../../api/AppUrl';
+import axios from 'axios';
 
 class UserRegister extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            name: "",
+            email: "",
+            password: "",
+            password_confirmation: "",
+            loggedIn: false
+        }
+    }
+
+    formSubmit = (e) => {
+        e.preventDefault();
+        const data = {
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password,
+            password_confirmation: this.state.password_confirmation,
+        }
+
+        axios.post(AppUrl.UserRegister, data)
+            .then(response => {
+                localStorage.setItem("token", response.data.token)
+                this.setState({ loggedIn: true })
+
+            })
+
+
+    }
+
     render() {
+            //after Register 
+    if(this.state.loggedIn) {
+        return <Navigate to={'/profile'}/>
+      }
         return (
             <Fragment>
                 <Container>
@@ -13,13 +50,13 @@ class UserRegister extends Component {
 
                             <Row className="text-center">
                                 <Col className="d-flex justify-content-center" lg={6} md={6} sm={12} xs={12}>
-                                    <Form className='onboardForm'>
+                                    <Form className='onboardForm' onSubmit={this.formSubmit}>
                                         <h4 className="section-title-login"> USER REGISTER </h4>
-                                        <input className="form-control m-2" type="text" placeholder="Enter your name" />
-                                        <input className="form-control m-2" type="text" placeholder="Enter your email" />
-                                        <input className="form-control m-2" type="text" placeholder="Enter your password" />
-                                        <input className="form-control m-2" type="text" placeholder="Renter your password" />
-                                        <Button className="btn btn-block m-2 site-btn-login"> Register </Button>
+                                        <input className="form-control m-2" onChange={(e) => { this.setState({ name: e.target.value }) }} type="text" placeholder="Enter your name" />
+                                        <input className="form-control m-2" onChange={(e) => { this.setState({ email: e.target.value }) }} type="text" placeholder="Enter your email" />
+                                        <input className="form-control m-2" onChange={(e) => { this.setState({ password: e.target.value }) }} type="text" placeholder="Enter your password" />
+                                        <input className="form-control m-2" onChange={(e) => { this.setState({ password_confirmation: e.target.value }) }} type="text" placeholder="Renter your password" />
+                                        <Button type='submit' className="btn btn-block m-2 site-btn-login"> Register </Button>
                                         <br></br><br></br>
                                         <hr />
                                         <p>Already Have An Account? <Link to="/login">Click Here to Login</Link></p>
