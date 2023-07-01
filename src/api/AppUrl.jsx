d32139f7c9f1f1e8485a3249cd2b4247838da5cc
ghost173 <jpirakas007@gmail.com>
2023-07-01 22:22:32 +0530

    minor fix

--- src/api/AppUrl.jsx
+++ src/api/AppUrl.jsx
@@ -1,5 +1,5 @@
 class AppUrl {
-  static BaseURL = "http://127.0.0.1:8000/api"
+  static BaseURL = "https://pwaadmin.mydevdemo.xyz/api"
   static VisitorDetails = this.BaseURL + "/createvisitor"
   static contactpost = this.BaseURL + "/postcontact"
   static allsiteinfo = this.BaseURL + "/allsiteinfo"
